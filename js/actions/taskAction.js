62c353aa50d494c58ed4264f0070b4161ccbd1af
tarabai.sortur <tarasortur@gmail.com>
2018-12-04 19:26:29 +0530

    changes in progress

---
+++ js/actions/taskAction.js
@@ -0,0 +1,2 @@
+import * as Constants from '../constants/constants';
+
---
+++ js/constants/constants.js
@@ -0,0 +1,3 @@
+export default const ADD_TASK = 'ADD_TASK';
+export default const DELETE_TASK = "DELETE_TASK";
+export default const UPDATE_TASK = "UPDATE_TASK";
